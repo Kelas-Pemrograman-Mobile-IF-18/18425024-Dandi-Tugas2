3b0c002a21812d95fc9a986204190b5eef78b970
dandi1842 <dandi.18425024@student.ubl.ac.id>
2021-03-30 23:02:13 +0700

    tugas2ku

--- app/src/main/java/com/dandikurniawan/tugas1dandi/MainActivity.java
+++ app/src/main/java/com/dandikurniawan/tugas1dandi/MainActivity.java
@@ -3,18 +3,41 @@ package com.dandikurniawan.tugas1dandi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtNama;
+    TextView txtTampil;
+    EditText edtNpm, edtNama, edtJenisKelamin, edtNoTelp;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNama = (TextView) findViewById(R.id.txtNama);
-        txtNama.setText("Dandi Kurniawan");
+        txtTampil = (TextView) findViewById(R.id.txtNpmNamaJenisKelaminNoTelp);
+
+        edtNpm = (EditText) findViewById(R.id.edtNpm);
+        edtNama = (EditText) findViewById(R.id.edtNama);
+        edtJenisKelamin = (EditText) findViewById(R.id.edtJenisKelamin);
+        edtNoTelp = (EditText) findViewById(R.id.edtNoTelp);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNpm = edtNpm.getText().toString();
+                String strNama = edtNama.getText().toString();
+                String strJenisKelamin = edtJenisKelamin.getText().toString();
+                String strNoTelp = edtNoTelp.getText().toString();
+
+                txtTampil.setText(strNpm + "\n" + strNama + "\n" + strJenisKelamin + "\n" + strNoTelp);
+
+            }
+        });
     }
 }
